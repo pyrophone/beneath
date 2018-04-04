919a5e91e0f8e078525ae433092ade0a08e75625
Nezkull <nick.marano4@gmail.com>
2018-04-04 18:10:03 -0400

    ARPuzzleControl script started
    The ARPuzzleControl script was started and canvas content was added to the scene as well. The canvas is not active until after the player has clicked or tapped the coin, after which the coin will disappear an the canvas will appear. The button currently does nothing. Style and other artistic content will have to be added to the canvas in the scene.

---
+++ Assets/Scripts/ARPuzzleControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ARPuzzleControl : MonoBehaviour
+{
+
+    public GameObject canvas;
+    public Button bttn;
+
+    // Use this for initialization
+    void Start ()
+    {
+        canvas.SetActive(false); // makes sure that the canvas is deactivated when the camera is opened
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject); // destroys the coin for from the image target
+        
+        canvas.SetActive(true); // reactivates the canvas
+
+        /*
+        Button btn = bttn.GetComponent<Button>();
+        btn.onClick.AddListener(Clicked); // this doesn't work
+        Debug.Log("event listener added");
+        */
+    }
+    
+    /*
+    // this doesn't work
+    private void Clicked()
+    {
+        Debug.Log("Closed dat shizz doh");
+    }
+    */
+}
