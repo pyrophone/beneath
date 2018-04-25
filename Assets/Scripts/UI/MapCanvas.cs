052915638f1df2c6d6ee488f7161472beb3c0354
Adrian Chiavelli Hernandez <arc8440@rit.edu>
2018-04-25 04:16:17 +0200

    Integrated arpuzzle branch
    Integrated arpuzzle branch, minimal changes to MapCanvas class for compatability. If there are still issues, there will probably be issues in the other canvases, UIControl, or the tutorial classes.

--- Assets/Scripts/UI/MapCanvas.cs
+++ Assets/Scripts/UI/MapCanvas.cs
@@ -4,11 +4,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-/*! \class MapCanvas
- *	\brief Handles the map canvas
- */
 public class MapCanvas : AbstractCanvas
 {
+	// private UIControl uiControl; //! The UIControl component
 	private Button qListButton; //! The button for the quest list screen
 	private Button playerButton; //! The next button for the player screen
 	private Button settingsButton; //! The next button for the settings screen
@@ -22,86 +20,99 @@ public class MapCanvas : AbstractCanvas
 
     /*! \brief Called when the object is initialized
 	 */
-	protected override void Awake()
-	{
-		base.Awake();
 
-	 	qListButton = transform.Find("QuestListButton").GetComponent<Button>();
-		qListButton.onClick.AddListener(OnQListButtonClick);
-		playerButton = transform.Find("PlayerButton").GetComponent<Button>();
-		playerButton.onClick.AddListener(OnPlayerButtonClick);
-		settingsButton = transform.Find("SettingsButton").GetComponent<Button>();
-		settingsButton.onClick.AddListener(OnSettingsButtonClick);
+    protected override void Awake()
+    {
+        base.Awake();
+
+        qListButton = transform.Find("QuestListButton").GetComponent<Button>();
+        qListButton.onClick.AddListener(OnQListButtonClick);
+        playerButton = transform.Find("PlayerButton").GetComponent<Button>();
+        playerButton.onClick.AddListener(OnPlayerButtonClick);
+        settingsButton = transform.Find("SettingsButton").GetComponent<Button>();
+        settingsButton.onClick.AddListener(OnSettingsButtonClick);
         locButton = transform.Find("GeoButton").GetComponent<Button>();
         locButton.onClick.AddListener(OnGeoClick);
 
+        nameText = playerButton.transform.Find("PlayerName").GetComponent<Text>();
+        lvlText = playerButton.transform.Find("PlayerLevel").GetComponent<Text>();
+        expText = playerButton.transform.Find("PlayerXP").GetComponent<Text>();
+
+        /*
+        nameText = transform.Find("PlayerName").GetComponent<Text>();
+        lvlText = transform.Find("PlayerLevel").GetComponent<Text>();
+        expText = transform.Find("PlayerXP").GetComponent<Text>();
+        */
+    }
+
+    private void Start()
+	{
+        /*
+		// uiControl = transform.parent.GetComponent<UIControl>();
+        
         if(playerButton != null)
         {
             nameText =  playerButton.transform.Find("PlayerName").GetComponent<Text>();
             lvlText =   playerButton.transform.Find("PlayerLevel").GetComponent<Text>();
             expText =   playerButton.transform.Find("PlayerXP").GetComponent<Text>();
         }
-    }
-
-	/*! \brief Called when the object is initialized
-	 */
-	private void Start()
-	{
+        */
         player = GameObject.Find("player").GetComponent<Player>();
-	}
+        
+    }
 
 	/*! \brief Updates the object
 	 */
 	protected override void Update()
 	{
-        nameText.text = player.PName;
-        lvlText.text = "LVL:" + player.Lvl;
-        expText.text = "EXP: " + player.Exp;
+        nameText.text = player.PName; // get the player name
+        lvlText.text = "LVL: " + player.LVL; // get the player level
+        expText.text = "EXP: " + player.EXP; // get the player exp
     }
 
     /*! \brief Called when the location text is clicked
 	 */
     private void OnGeoClick()
     {
-        //solution from https://github.com/sanukin39/UniClipboard
+        //solution from https://github.com/sanukin39/UniClipboard    
         UniClipboard.SetText(transform.Find("GeoCounter").GetComponent<Text>().text);
         Handheld.Vibrate();
     }
 
-	/*! \brief Updates the ui for the tutorial
+    /*! \brief Updates the ui for the tutorial
 	 */
-	public override void UpdateTutorialUI()
-	{
-		if(uiControl.TutorialActive)
-		{
-			playerButton.interactable = false;
-			settingsButton.interactable = false;
-		}
+    public override void UpdateTutorialUI()
+    {
+        if (uiControl.TutorialActive)
+        {
+            playerButton.interactable = false;
+            settingsButton.interactable = false;
+        }
 
-		else
-		{
-			playerButton.interactable = true;
-			settingsButton.interactable = true;
-		}
-	}
+        else
+        {
+            playerButton.interactable = true;
+            settingsButton.interactable = true;
+        }
+    }
 
-	/*! \brief Called when the quest list button is clicked
+    /*! \brief Called when the quest list button is clicked
 	 */
     private void OnQListButtonClick()
 	{
-		if(uiControl.TutorialActive)
-		{
-			TutorialOverlay to = transform.Find("../TutorialOverlay").GetComponent<TutorialOverlay>();
-			if(to.TutorialProgress == 2)
-			{
-				to.SpecialClick();
-				uiControl.SetCanvas(UIState.QLIST);
-			}
-		}
+        if (uiControl.TutorialActive)
+        {
+            TutorialOverlay to = transform.Find("../TutorialOverlay").GetComponent<TutorialOverlay>();
+            if (to.TutorialProgress == 2)
+            {
+                to.SpecialClick();
+                uiControl.SetCanvas(UIState.QLIST);
+            }
+        }
 
-		else
-			uiControl.SetCanvas(UIState.QLIST);
-	}
+        else
+            uiControl.SetCanvas(UIState.QLIST);
+    }
 
 	/*! \brief Called when the player button is clicked
 	 */
--- Assets/Scripts/UI/PlayerCanvas.cs
+++ Assets/Scripts/UI/PlayerCanvas.cs
@@ -4,13 +4,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-/*! \class PlayerCanvas
- *	\brief Handles the player canvas
- */
 public class PlayerCanvas : AbstractCanvas
-{
+{ 
 	private Button backButton; //! Reference to the back button
-	private Button playerButton; //! Button for the player to change their name
 
     private Text nameText;  //! The Text component related to the player's name
     private Text lvlText;   //! The Text component related to the player's level
@@ -18,37 +14,32 @@ public class PlayerCanvas : AbstractCanvas
 
     private Player player; //! The instantiated prefab of Player
 
-    /*! \brief Called on startup
-	 */
-	protected override void Awake()
-	{
-		base.Awake();
+    protected override void Awake()
+    {
+        base.Awake();
 
-		backButton = transform.Find("BackButton").GetComponent<Button>();
+        backButton = transform.Find("BackButton").GetComponent<Button>();
 		backButton.onClick.AddListener(OnBackButtonClick);
 
-		playerButton = transform.Find("PlayerName").GetComponent<Button>();
-		playerButton.onClick.AddListener(OnPlayerButtonClick);
-
-        nameText = transform.Find("PlayerName").Find("Text").GetComponent<Text>();
+        nameText = transform.Find("PlayerName").GetComponentInChildren<Text>();
         lvlText = transform.Find("PlayerLevel").GetComponent<Text>();
         expText = transform.Find("PlayerXP").GetComponent<Text>();
     }
 
-	/*! \brief Called when the object is initialized
+    /*! \brief Called when the object is initialized
 	 */
-	private void Start()
+    private void Start()
 	{
         player = GameObject.Find("player").GetComponent<Player>();
-	}
+    }
 
-	/*! \brief Updates the object
+    /*! \brief Updates the object
 	 */
-	protected override void Update()
+    protected override void Update()
 	{
-        nameText.text = player.PName;
-        lvlText.text = "LVL:" + player.Lvl;
-        expText.text = "EXP: " + player.Exp;
+        nameText.text = player.PName; // get the player name
+        lvlText.text = "LVL:" + player.LVL; // get the player level
+        expText.text = "EXP: " + player.EXP; // get the player exp
     }
 
 	/*! \brief Called when the back button is clicked
@@ -56,12 +47,5 @@ public class PlayerCanvas : AbstractCanvas
 	private void OnBackButtonClick()
 	{
 		uiControl.SetCanvas(UIState.MAP);
-	}
-
-	/*! \brief called when the player clicks on their name
-	 */
-	private void OnPlayerButtonClick()
-	{
-		transform.parent.Find("NameEnterCanvas").gameObject.SetActive(true);
 	}
 }
